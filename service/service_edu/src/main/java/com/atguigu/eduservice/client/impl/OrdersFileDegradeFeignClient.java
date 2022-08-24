6d69249bc9e8f9b1e5fc7fa1686e69e494a4d0ff
13178356705 <2378007105@qq.com>
2022-08-24 16:19:41 +0800

    1

--- service/service_edu/src/main/java/com/atguigu/eduservice/client/impl/OrdersFileDegradeFeignClient.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/client/impl/OrdersFileDegradeFeignClient.java
@@ -2,7 +2,7 @@ package com.atguigu.eduservice.client.impl;
 
 import com.atguigu.eduservice.client.OrdersClient;
 
-public class OrdersClientFileDegradeFeignClient implements OrdersClient {
+public class OrdersFileDegradeFeignClient implements OrdersClient {
     @Override
     public boolean isBuyCourse(String courseId, String memberId) {
         return false;
