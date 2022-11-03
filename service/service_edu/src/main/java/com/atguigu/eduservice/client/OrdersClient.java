a89fbc8c3ca8d074c7079c569f6e523d837eaa2b
test <2378007105@qq.com>
2022-11-03 12:37:51 +0800

    Revert "1"
    This reverts commit 8ca0c58512dfed4bc34d8aa307e6d327d9f7aea4.

--- service/service_edu/src/main/java/com/atguigu/eduservice/client/OrdersClient.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/client/OrdersClient.java
@@ -1,13 +1,12 @@
 package com.atguigu.eduservice.client;
 
-import com.atguigu.eduservice.client.impl.OrdersFileDegradeFeignClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Component
-@FeignClient(name = "service-order",fallback = OrdersFileDegradeFeignClient.class)
+@ComponentScan
+@FeignClient("service-order")
 public interface OrdersClient {
 
     //根据课程id和用户id查询订单表中订单状态
--- service/service_edu/src/main/java/com/atguigu/eduservice/client/impl/OrdersClientFileDegradeFeignClient.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/client/impl/OrdersClientFileDegradeFeignClient.java
@@ -2,7 +2,7 @@ package com.atguigu.eduservice.client.impl;
 
 import com.atguigu.eduservice.client.OrdersClient;
 
-public class OrdersFileDegradeFeignClient implements OrdersClient {
+public class OrdersClientFileDegradeFeignClient implements OrdersClient {
     @Override
     public boolean isBuyCourse(String courseId, String memberId) {
         return false;
